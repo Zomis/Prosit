3e93e0b3beb487e696d24b86fb84f3e34d5e4a4d
Simon Forsberg <zomis2k@hotmail.com>
2015-01-06 23:39:45 +0100

    extracted fragment to its own file

---
+++ prosit/src/main/java/net/zomis/prosit/SneezeFragment.java
@@ -0,0 +1,24 @@
+package net.zomis.prosit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment containing a simple button.
+ */
+public class SneezeFragment extends Fragment {
+
+    public SneezeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        
+        return rootView;
+    }
+}
